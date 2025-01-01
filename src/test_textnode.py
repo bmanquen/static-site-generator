77e8d17eb24d9d8b7e2308ba6a9e0680eda9f20c
bmanquen <bmanquen1993@gmail.com>
2025-01-01 06:32:46 -0600

    added TextNode

---
+++ src/test_textnode.py
@@ -0,0 +1,30 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = TextNode("This is a test node", TextType.ITALIC)
+        node2 = TextNode("This is a test", TextType.CODE)
+        self.assertNotEqual(node, node2)
+
+    def test_url_none(self):
+        node = TextNode("This is a test", TextType.CODE)
+        self.assertIsNone(node.url)
+
+        node.url = "www.example.com"
+        self.assertIsNotNone(node.url)
+
+    def test_testtype_enum(self):
+        enums = list(TextType)
+        with self.assertRaises(AttributeError):
+            node = TextNode("this is a test", TextType.FAKE)
+
+if __name__ == "__main__":
+    unittest.main()
