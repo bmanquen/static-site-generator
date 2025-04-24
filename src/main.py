3cd74da29e85167ea3c0ccd5851925fdd90b0832
bmanquen <bmanquen1993@gmail.com>
2025-04-24 06:16:51 -0500

    feat: configure to deploy with Github Pages

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 from pathlib import Path
 
 from markdown_to_html import *
@@ -8,47 +9,13 @@ from utils import extract_title
 
 
 def main():
-    text_node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    extract_title(
-        """# Tolkien Fan Club
-
-**I like Tolkien**. Read my [first post here](/majesty) (sorry the link doesn't work yet)
-
-> All that is gold does not glitter
-
-## Reasons I like Tolkien
-
-* You can spend years studying the legendarium and still not understand its depths
-* It can be enjoyed by children and adults alike
-* Disney *didn't ruin it*
-* It created an entirely new genre of fantasy
-
-## My favorite characters (in order)
-
-1. Gandalf
-2. Bilbo
-3. Sam
-4. Glorfindel
-5. Galadriel
-6. Elrond
-7. Thorin
-8. Sauron
-9. Aragorn
-
-Here's what `elflang` looks like (the perfect coding language):
-
-```
-func main(){
-    fmt.Println("Hello, World!")
-}
-```"""
-    )
-    copy_contents("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    copy_contents("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 
 def copy_contents(source, destination):
-    if os.path.exists(destination) and destination == "public":
+    if os.path.exists(destination) and destination == "docs":
         shutil.rmtree(destination)
     os.mkdir(destination)
     for filename in os.listdir(source):
@@ -60,7 +27,7 @@ def copy_contents(source, destination):
             copy_contents(from_path, dest_path)
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     markdown = open(from_path).read()
     template = open(template_path).read()
     html_string = markdown_to_html_node(markdown).to_html()
@@ -68,6 +35,8 @@ def generate_page(from_path, template_path, dest_path):
 
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html_string)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
 
     dest_dirs, file = os.path.split(dest_path)
     dest_path = dest_path.replace(".md", ".html")
@@ -76,17 +45,20 @@ def generate_page(from_path, template_path, dest_path):
         file.write(template)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     files = os.listdir(dir_path_content)
     for file in files:
         if os.path.isfile(f"{dir_path_content}/{file}"):
             generate_page(
-                f"{dir_path_content}/{file}", template_path, f"{dest_dir_path}/{file}"
+                f"{dir_path_content}/{file}",
+                template_path,
+                f"{dest_dir_path}/{file}",
+                basepath,
             )
         else:
             dest = os.path.join(dest_dir_path, file)
             generate_pages_recursive(
-                os.path.join(dir_path_content, file), template_path, dest
+                os.path.join(dir_path_content, file), template_path, dest, basepath
             )
 
 
